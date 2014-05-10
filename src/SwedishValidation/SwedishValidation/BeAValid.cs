6f125225ad6edf5fbd611b93dbd163cb40f91034
Fredrik Eriksson <ein_baver@hotmail.com>
2014-05-10 16:28:59 +0200

    Added som projects

---
+++ src/SwedishValidation/SwedishValidation/BeAValid.cs
@@ -0,0 +1,20 @@
+﻿namespace SwedishValidation
+{
+    public class BeAValid
+    {
+        public static bool PhoneNumber(string phoneNumber)
+        {
+            return true;
+        }
+
+        public static bool MobilePhoneNumber(string mobilePhoneNumber)
+        {
+            return true;
+        }
+
+        public static bool SocialSecurityNumber(string socialSecurityNumber)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/SwedishValidation/SwedishValidation/FormatThis.cs
@@ -0,0 +1,20 @@
+﻿namespace SwedishValidation
+{
+    public class FormatThis
+    {
+        public static string MobilePhoneNumber(string mobilePhoneNumber)
+        {
+            return string.Empty;
+        }
+
+        public static string PhoneNumber(string mobilePhoneNumber)
+        {
+            return string.Empty;
+        }
+
+        public static string SocialSecurityNumber(string mobilePhoneNumber)
+        {
+            return string.Empty;
+        }
+    }
+}
